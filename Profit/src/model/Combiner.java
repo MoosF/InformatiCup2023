094970d40280837cf3981840fb5906f92fea78e2
YKraml <82815144+YKraml@users.noreply.github.com>
2022-11-27 15:31:11 +0100

    Kleine Refactorings an movable object

--- Profit/src/model/Combiner.java
+++ Profit/src/model/Combiner.java
@@ -9,7 +9,7 @@ import java.util.Map;
  */
 public class Combiner extends MovableObject {
 
-  private CombinerSubType subType;
+  private final CombinerSubType subType;
 
   /**
    * Constructor of {@link Combiner}.
@@ -30,60 +30,50 @@ public class Combiner extends MovableObject {
    * @param horPos  X-Coordinate of the {@link Combiner}.
    * @param verPos  Y-Coordinate of the {@link Combiner}.
    * @param subtype Subtype of the {@link Combiner}.
-   * @return New instnace of {@link Combiner}.
+   * @return New instance of {@link Combiner}.
    */
   public static Combiner createCombiner(int horPos, int verPos, CombinerSubType subtype) {
 
-    Tile[] tiles = null;
+    Tile[] tiles;
     switch (subtype) {
 
-      case OUTPUT_EAST -> {
-        tiles = new Tile[]{
-            new Tile(-1, -1, TileType.INPUT),
-            new Tile(-1, 0, TileType.INPUT),
-            new Tile(-1, 1, TileType.INPUT),
-            new Tile(0, -1, TileType.SOLID),
-            new Tile(0, 0, TileType.SOLID),
-            new Tile(0, 1, TileType.SOLID),
-            new Tile(1, 0, TileType.OUTPUT)
-        };
-      }
-      case OUTPUT_SOUTH -> {
-        tiles = new Tile[]{
-            new Tile(-1, -1, TileType.INPUT),
-            new Tile(0, -1, TileType.INPUT),
-            new Tile(1, -1, TileType.INPUT),
-            new Tile(-1, 0, TileType.SOLID),
-            new Tile(0, 0, TileType.SOLID),
-            new Tile(1, 0, TileType.SOLID),
-            new Tile(0, 1, TileType.OUTPUT)
-        };
-      }
-      case OUTPUT_WEST -> {
-        tiles = new Tile[]{
-            new Tile(1, -1, TileType.INPUT),
-            new Tile(1, 0, TileType.INPUT),
-            new Tile(1, 1, TileType.INPUT),
-            new Tile(0, -1, TileType.SOLID),
-            new Tile(0, 0, TileType.SOLID),
-            new Tile(0, 1, TileType.SOLID),
-            new Tile(-1, 0, TileType.OUTPUT)
-        };
-      }
-      case OUTPUT_NORTH -> {
-        tiles = new Tile[]{
-            new Tile(-1, 1, TileType.INPUT),
-            new Tile(0, 1, TileType.INPUT),
-            new Tile(1, 1, TileType.INPUT),
-            new Tile(-1, 0, TileType.SOLID),
-            new Tile(0, 0, TileType.SOLID),
-            new Tile(1, 0, TileType.SOLID),
-            new Tile(0, -1, TileType.OUTPUT)
-        };
-      }
-      default -> {
-        throw new RuntimeException("Unknown branch.");
-      }
+      case OUTPUT_EAST -> tiles = new Tile[]{
+          new Tile(-1, -1, TileType.INPUT),
+          new Tile(-1, 0, TileType.INPUT),
+          new Tile(-1, 1, TileType.INPUT),
+          new Tile(0, -1, TileType.SOLID),
+          new Tile(0, 0, TileType.SOLID),
+          new Tile(0, 1, TileType.SOLID),
+          new Tile(1, 0, TileType.OUTPUT)
+      };
+      case OUTPUT_SOUTH -> tiles = new Tile[]{
+          new Tile(-1, -1, TileType.INPUT),
+          new Tile(0, -1, TileType.INPUT),
+          new Tile(1, -1, TileType.INPUT),
+          new Tile(-1, 0, TileType.SOLID),
+          new Tile(0, 0, TileType.SOLID),
+          new Tile(1, 0, TileType.SOLID),
+          new Tile(0, 1, TileType.OUTPUT)
+      };
+      case OUTPUT_WEST -> tiles = new Tile[]{
+          new Tile(1, -1, TileType.INPUT),
+          new Tile(1, 0, TileType.INPUT),
+          new Tile(1, 1, TileType.INPUT),
+          new Tile(0, -1, TileType.SOLID),
+          new Tile(0, 0, TileType.SOLID),
+          new Tile(0, 1, TileType.SOLID),
+          new Tile(-1, 0, TileType.OUTPUT)
+      };
+      case OUTPUT_NORTH -> tiles = new Tile[]{
+          new Tile(-1, 1, TileType.INPUT),
+          new Tile(0, 1, TileType.INPUT),
+          new Tile(1, 1, TileType.INPUT),
+          new Tile(-1, 0, TileType.SOLID),
+          new Tile(0, 0, TileType.SOLID),
+          new Tile(1, 0, TileType.SOLID),
+          new Tile(0, -1, TileType.OUTPUT)
+      };
+      default -> throw new RuntimeException("Unknown branch.");
     }
 
     return new Combiner(horPos, verPos, tiles, MovableObjectType.COMBINER, subtype);
